f757cad033d5e8b55c75840ecf4d93ebdd864779
Nephtali Kasela <nephtalikasela04@gmail.com>
2022-12-11 17:39:52 +0300

    make a small change

--- Task2/Aircraft.cs
+++ Task2/Aircraft.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Airport
-{                                                           // gregor
+{                                                          
     internal class Aircraft
     {
         private string name = String.Empty;
